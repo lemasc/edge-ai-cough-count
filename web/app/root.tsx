4a53eff025c061d3a31096b026593f19dcc62ec8
Lemasc Service <lemascserviceth@hotmail.com>
2026-02-26 22:18:09 +0700

    feat: update font

--- web/app/root.tsx
+++ web/app/root.tsx
@@ -25,13 +25,13 @@ export const links: Route.LinksFunction = () => [
   },
   {
     rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+    href: "https://fonts.googleapis.com/css2?family=Noto+Color+Emoji&family=Noto+Sans+Thai:wght@100..900&display=swap",
   },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="th">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
