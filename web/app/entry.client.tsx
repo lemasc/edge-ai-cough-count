5fb8735001fddf6ef0f6fe8dc192e71b8e6fafed
Lemasc Service <lemascserviceth@hotmail.com>
2026-02-28 17:07:02 +0700

    feat: add logrocket

--- web/app/entry.client.tsx
+++ web/app/entry.client.tsx
@@ -3,12 +3,17 @@ import posthog from "posthog-js";
 import { startTransition, StrictMode } from "react";
 import { hydrateRoot } from "react-dom/client";
 import { HydratedRouter } from "react-router/dom";
+import LogRocket from "logrocket";
 
 posthog.init(import.meta.env.VITE_PUBLIC_POSTHOG_TOKEN, {
   api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
   defaults: "2025-05-24",
   capture_exceptions: true,
 });
+
+if (import.meta.env.PROD && import.meta.env.VITE_PUBLIC_LOGROCKET_APP_ID) {
+  LogRocket.init(import.meta.env.VITE_PUBLIC_LOGROCKET_APP_ID);
+}
 
 startTransition(() => {
   hydrateRoot(
